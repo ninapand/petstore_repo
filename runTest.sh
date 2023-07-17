eed4ad254dea8c1043536e83443f8be81986c782
Neena Pandey <neenapandey7@gmail.com>
2023-07-17 13:47:49 +1000

    updated runETst.sh to include new postman collection name

--- runTest.sh
+++ runTest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-newman run collection/Swagger_Petstore_env_slackmsg.postman_collection.json -d data/petstore_order.csv -e data/qa.postman_environment.json
+newman run collection/Swagger_Petstore_env_slacknotifi_final.postman_collection.json -d data/petstore_order.csv -e data/qa.postman_environment.json
